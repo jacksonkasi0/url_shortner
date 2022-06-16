497f72020632e9a1132e885ea3ea33f57d65f44d
Jackson Kasi <jacksonkasi@outlook.com>
2022-06-16 14:51:31 +0000

    update

--- frontend/src/Pages/UrlDetails/UrlDetails.js
+++ frontend/src/Pages/UrlDetails/UrlDetails.js
@@ -10,40 +10,14 @@ import {
   UilTwitterAlt,
   UilExternalLinkAlt,
 } from "@iconscout/react-unicons";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Pie } from "react-chartjs-2";
+// import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+// import { Pie } from "react-chartjs-2";
 
 import Input from "../../components/Input/Input";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+// ChartJS.register(ArcElement, Tooltip, Legend);
 
 const UrlDetails = ({ urlCode }) => {
-  const data = {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
 
   const { urlList } = useSelector((state) => state.app);
   const [isOpen, setIsOpen] = useState(false);
@@ -122,9 +96,9 @@ const UrlDetails = ({ urlCode }) => {
       <Box sx={{ mt: 5 }}>
         <p>Total Clicks = {url.clicks}</p>
       </Box>
-      <div style={{width:"90px"}}>    
+      {/* <div style={{width:"90px"}}>    
         <Pie data={data} />
-      </div>
+      </div> */}
     </div>
   );
 };
